3f7d2210436eaf7d21ec407e46a3ddad86c7d1fa
Olaf Klischat <o.klischat@syseleven.de>
2020-05-16 12:55:43 +0200

    ubuntu/icewm container compat w/ Ubuntu 20.04

--- src/ubuntu/install/icewm_ui.sh
+++ src/ubuntu/install/icewm_ui.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Install IceWM UI components"
 apt-get update 
-apt-get install -y supervisor icewm xterm xfonts-base xauth xinit
+apt-get install -y supervisor icewm xterm xfonts-base xauth xinit libgtk-3-0
 apt-get purge -y pm-utils xscreensaver*
 apt-get clean -y
--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -8,7 +8,9 @@ apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy #used for websockify/novnc
 apt-get clean -y
 
-ln -s /usr/bin/python2 /usr/bin/python
+if [[ ! -e /usr/bin/python ]]; then
+    ln -s /usr/bin/python2 /usr/bin/python
+fi
 
 echo "generate locales für en_US.UTF-8"
 locale-gen en_US.UTF-8
